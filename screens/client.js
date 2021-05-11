8015e2dcf0895f9a1dbb96a8c8a7c172b9fd5c37
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-11 15:27:51 -0500

    Render items and navigation in client screen
    * Initial configuration for app navigation
    
    * Work in screen client
    
    * Request to pets to client logged
    
    * Implement the information Pet in screen
    
    * Screen Client navigation and render items

--- screens/client.js
+++ screens/client.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import { SafeAreaView, FlatList, Pressable, Text, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { links } from '../linksClient';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  SafeAreaView,
+  FlatList,
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+import { ListItemPet } from '../components/ListItemPet';
 
 function Client() {
+  const [token, setToken] = useState('');
+  const [ownPets, setOwnPets] = useState([]);
 
-  return (
+  useEffect(() => {
+    AsyncStorage.getItem('token').then((token) => {
+      setToken(token);
+
+      axios({
+        method: 'GET',
+        baseURL: 'http://192.168.10.12:8000',
+        url: '/pets',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then(({ data: { pets } }) => {
+          setOwnPets(pets)
+        })
+        .catch((error) => console.log(error));
+    });
+  }, [token]);
+
+  return !!ownPets && (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={links}
+        data={ownPets}
         renderItem={({ item }) => (
-          <Pressable
-            style={styles.item}
-            onPress={() => alert(item.name)}
-            >
-            <Text>
-              <Ionicons name={item.icon} size={32} color="black" />
-            </Text>
+          <View>
             <Text>{item.name}</Text>
-            <Text>
-              <Ionicons name="arrow-forward" size={24} color="black" />{' '}
-            </Text>
-          </Pressable>
-
+            <ListItemPet item={item}/>
+          </View>
         )}
-
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item._rid}
       />
     </SafeAreaView>
   );
@@ -42,6 +60,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-  }
+  },
 });
 export default Client;
