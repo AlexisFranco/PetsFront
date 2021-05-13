06b950f94986d7e7dee827172f97a51ac5d5f18b
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-12 22:09:43 -0500

    Create Register

--- screens/register.js
+++ screens/register.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { REACT_NATIVE_SERVER_URL } from '@env';
 import { useNavigation } from '@react-navigation/native';
+import RNPickerSelect from 'react-native-picker-select';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import {
@@ -17,12 +18,35 @@ function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [phone, setPhone] = useState(0);
+  const [phoneNum, setPhoneNum] = useState('');
+  const [selectedTypeUser, setSelectedTypeUser] = useState('');
+
   const SERVER_URL = REACT_NATIVE_SERVER_URL;
   const navigation = useNavigation();
 
   function handleSubmit() {
-
+    axios({
+      method: 'POST',
+      baseURL: SERVER_URL,
+      url: `/${selectedTypeUser}`,
+      data: {
+        name,
+        email,
+        password,
+        phoneNum,
+      },
+    })
+      .then(({ data: { token, userType, userID } }) => {
+        if (userType === 'client') {
+          AsyncStorage.setItem('token', token);
+          navigation.navigate('Inicio');
+        } else {
+          console.log(userID)
+          AsyncStorage.setItem('token', token);
+          navigation.navigate('Paseador', { userID });
+        }
+      })
+      .catch((error) => console.dir(error));
   }
 
   return (
@@ -55,9 +79,20 @@ function Register() {
         <TextInput
           style={styles.textInput}
           placeholder="Teléfono"
-          onChangeText={(num) => setPhone(num)}
-          value={phone}
+          onChangeText={(text) => setPhoneNum(text)}
+          value={phoneNum}
           keyboardType="numeric"
+        />
+        <RNPickerSelect
+          onValueChange={(value) => setSelectedTypeUser(value)}
+          placeholder={{
+            label: 'Selecciona tu usuario',
+            value: null,
+          }}
+          items={[
+            { label: 'Usuario', value: 'clients' },
+            { label: 'Paseador', value: 'walkers' },
+          ]}
         />
         <Button title="Registrarse" onPress={handleSubmit} />
       </SafeAreaView>
