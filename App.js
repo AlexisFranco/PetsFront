629480dd893047f5949514d9c29d489283f78f5a
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-12 20:15:12 -0500

    Create screen for the personal information of pet owner

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack'
+
 import Login from './screens/login';
 import Client from './screens/client';
-import InformationPet from './screens/informationPet';
-import InformationMedicine from './screens/informationMedicine';
-import InformationWalker from './screens/informationWalker';
 import Walker from './screens/walker';
+import InformationPet from './screens/informationPet';
+import InformationClient from './screens/informationClient';
+import InformationWalker from './screens/informationWalker';
+import InformationMedicine from './screens/informationMedicine';
 
 const Stack = createStackNavigator();
 
@@ -14,12 +16,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Ingreso' component={Login}/>
-        <Stack.Screen name='Inicio' component={Client}/>
-        <Stack.Screen name='Paseador' component={Walker}/>
-        <Stack.Screen name='Información' component={InformationPet}/>
-        <Stack.Screen name='Medicinas' component={InformationMedicine}/>
-        <Stack.Screen name='Paseadores' component={InformationWalker}/>
+        <Stack.Screen name='Ingreso' component={ Login }/>
+        <Stack.Screen name='Inicio' component={ Client }/>
+        <Stack.Screen name='Información' component={ InformationClient }/>
+        <Stack.Screen name='Paseador' component={ Walker }/>
+        <Stack.Screen name='Mascota' component={ InformationPet }/>
+        <Stack.Screen name='Medicinas' component={ InformationMedicine }/>
+        <Stack.Screen name='Paseadores' component={ InformationWalker }/>
       </Stack.Navigator>
     </NavigationContainer>
   );
