0085e15beca9820e6b9cc3adc642a7af2d6a4cdc
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-19 19:48:10 -0500

    List services owner walker in walker screen
    * Create screen for the personal information of pet owner
    
    * Initial configuration for register
    
    * List services list of walker in walker view

--- screens/walker.js
+++ screens/walker.js
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useRoute } from '@react-navigation/native';
 import { SafeAreaView, FlatList, View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,10 +9,10 @@ function Walker() {
   const idWalker = route.params.id;
   const dispatch = useDispatch()
 
-  const { loading, walker } = useSelector(
-    ({ usersReducer }) => ({
+  const { loading, walker, services } = useSelector(({ usersReducer }) => ({
       walker: usersReducer.walker,
       loading: usersReducer.loading,
+      services: usersReducer.walker.serviceIDs,
     })
   );
 
@@ -29,6 +28,22 @@ function Walker() {
         <Text>{walker.email}</Text>
         <Text>{walker.phoneNum}</Text>
         <Text>{walker.zone}</Text>
+        {!!services && (
+          <FlatList
+            data={services}
+            renderItem={({ item }) => (
+              <View>
+                <Text>Nombre mascota: {item.petID.name}</Text>
+                <Text>Dirección: {item.initLoc}</Text>
+                <Text>Fecha: {item.date}</Text>
+                <Text>Hora de paseo: {item.hour}</Text>
+                <Text>Tiempo de paseo: {item.time}</Text>
+                <Text>Estado del paseo: {item.status}</Text>
+              </View>
+            )}
+            keyExtractor={(item) => item._id}
+          />
+        )}
       </SafeAreaView>
     )
   );
