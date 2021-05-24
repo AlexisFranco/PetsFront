832a87b0d4b583720dd1abebe0e1a389d120d518
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-24 15:22:37 -0500

    Change style in login input

--- screens/login/styles.js
+++ screens/login/styles.js
@@ -8,13 +8,13 @@ export default StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: '#F3F2DC',
-    justifyContent: 'center',
   },
   textInput: {
     width: '85%',
     height: 40,
-    borderColor: '#D4E3E7',
-    borderBottomWidth: 1,
+    backgroundColor: '#FAF9F0',
+    borderRadius: 10,
+    paddingLeft: 10,
     marginBottom: 36,
     marginLeft: 15,
     marginRight: 15,
@@ -22,17 +22,20 @@ export default StyleSheet.create({
   image: {
     width: 200,
     height: 200,
+    marginTop: 50,
     marginBottom: 100,
   },
   button: {
     alignItems: 'center',
     backgroundColor: '#438E92',
+    borderRadius: 10,
     marginTop: 20,
+    marginBottom: 30,
     padding: 10,
     width: '85%',
   },
   register: {
-    marginTop: 100,
+    marginTop: 80,
     flexDirection: 'row',
   },
   textRegister: {
