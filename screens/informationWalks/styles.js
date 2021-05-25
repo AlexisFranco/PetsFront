ff989137469110cfc86bbd73580db00cba7c957f
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-25 08:01:46 -0500

    Styles the walks screen

---
+++ screens/informationWalks/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F2DC',
+  },
+  infoService: {
+    flex: 1,
+    width: '95%',
+    marginTop: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    paddingTop: 15,
+    paddingLeft: 40,
+    paddingBottom: 10,
+    flexDirection: 'row',
+    borderColor: '#438E92',
+    backgroundColor: '#FAF9F0',
+  },
+  infoColumn: {
+    flex:1,
+    flexDirection: 'column',
+  },
+  eachItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  text: {
+    marginLeft: 10,
+  },
+});
